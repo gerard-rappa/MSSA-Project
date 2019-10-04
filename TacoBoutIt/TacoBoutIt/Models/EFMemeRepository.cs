14c9b6849ea1fe15a29b283b8abb9ffeb0ca1050
Gerard Rappa <gerard.rappa@gmail.com>
2019-10-03 18:11:29 -0700

    Edits for efficiency

--- TacoBoutIt/TacoBoutIt/Models/EFMemeRepository.cs
+++ TacoBoutIt/TacoBoutIt/Models/EFMemeRepository.cs
@@ -15,6 +15,11 @@ namespace TacoBoutIt.Models
 
         public void Add(Meme meme)
         {
+            if(meme.Latitude == 0 && meme. Longitude == 0)
+            {
+                meme.Latitude = 71;
+                meme.Longitude = 25;
+            }
             context.Memes.Add(meme);
             context.SaveChanges();
         }
--- TacoBoutIt/TacoBoutIt/Models/Meme.cs
+++ TacoBoutIt/TacoBoutIt/Models/Meme.cs
@@ -6,6 +6,7 @@ namespace TacoBoutIt.Models
     public class Meme
     {
         public int MemeId { get; set; }
+        //public HttpPostedFile ImageFile { get; set; }
         public string ImgUrl { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
