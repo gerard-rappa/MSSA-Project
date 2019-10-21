63c960829bc0c9574b78e50a6eab8e30d4238689
Gerard Rappa <gerard.rappa@gmail.com>
2019-10-20 17:46:54 -0700

    Added link to map and displayed markers for all memes

--- TacoBoutIt/TacoBoutIt/Controllers/MapController.cs
+++ TacoBoutIt/TacoBoutIt/Controllers/MapController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TacoBoutIt.Models;
 
 namespace TacoBoutIt.Controllers
 {
     public class MapController : Controller
     {
+        private IMemeRepository repository;
+
+        public MapController(IMemeRepository repo)
+        {
+            repository = repo;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(repository.Memes);
         }
     }
 }
